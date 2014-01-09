9295eea22df6339e7fad701ed903a5d12521ade7
Jason Kulatunga <jk17@ualberta.ca>
2014-01-08 17:36:17 -0800

    update version.

--- matchmedia-ng.js
+++ matchmedia-ng.js
@@ -1,4 +1,4 @@
-/* matchmedia-ng v1.0.0 | (c) 2014 Jason Kulatunga, Inc. | http://analogj.mit-license.org/
+/* matchmedia-ng v1.0.1 | (c) 2014 Jason Kulatunga, Inc. | http://analogj.mit-license.org/
  */
 'use strict';
 
